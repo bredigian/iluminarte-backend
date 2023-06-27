8bc151239060305e17a2cfd14c185c4bf76b0490
Gianluca Bredice - Nitro 5 <bredicegian@gmail.com>
2023-06-27 15:52:11 -0300

    [fix] fix cors

--- constants/api.js
+++ constants/api.js
@@ -1,4 +1,5 @@
 const PORT = process.env.PORT || 3001
 const URL = process.env.URL || `http://localhost:${PORT}`
+const URL_FRONT = process.env.URL_FRONT || `http://localhost:3000`
 
-module.exports = { URL, PORT }
+module.exports = { URL, PORT, URL_FRONT }
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const cors = require("cors")
 
-const { PORT } = require("./constants/api")
+const { PORT, URL_FRONT } = require("./constants/api")
 const categoriesRoutes = require("./routes/categories")
 const productsRoutes = require("./routes/products")
 const authRoutes = require("./routes/auth")
@@ -12,6 +12,12 @@ require("dotenv").config()
 const app = express()
 
 app.use(cors())
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", URL_FRONT)
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization")
+  next()
+})
 app.use(express.json())
 app.use("/data", express.static("./data"))
 
