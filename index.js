83962041462a66b66ddace626806c3935301f600
Gianluca Bredice - Nitro 5 <bredicegian@gmail.com>
2023-07-03 17:43:31 -0300

    [change] change cors configuration

--- index.js
+++ index.js
@@ -12,12 +12,6 @@ require("dotenv").config()
 const app = express()
 
 app.use(cors())
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", URL_FRONT)
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization")
-  next()
-})
 app.use(express.json())
 app.use("/data", express.static("./data"))
 
